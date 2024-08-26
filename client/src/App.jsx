bb4661a2444376b6c910adebc7063fef8531ceb3
akash-panthri <akashpanthri2000@gmail.com>
2024-08-26 07:26:54 +0530

    Added Canvas, Home and Customizer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,6 @@
-
+import Canvas from "./canvas"
+import Customizer from "./pages/Customizer"
+import Home from "./pages/Home"
 
 
 function App() {
@@ -6,7 +8,13 @@ function App() {
 
   return (
     <>
-     Hello
+    <main className="app transition-all ease-in">
+
+    <Home />
+    <Canvas />
+    <Customizer />
+     
+    </main>
     </>
   )
 }
