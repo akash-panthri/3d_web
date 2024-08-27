4c7952a70a19ab1c24634ffe444064e8325c0bc4
akash-panthri <akashpanthri2000@gmail.com>
2024-08-27 07:30:22 +0530

    Added generateStyle function

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,6 +1,16 @@
+import { useSnapshot } from "valtio"
+import state from  "../store"
+// eslint-disable-next-line react/prop-types
+export default function CustomButton({type, title, customStyles,handleClick}) {
 
-export default function CustomButton() {
+  const snap = useSnapshot(state);
+
+  const generateStyle =(type)=>{
+    if(type === 'filled'){
+      return {backgroundColor: snap.color, color: 'white'}
+    }
+  }
   return (
-    <div>CustomButton</div>
+    <button style={generateStyle(type)} className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`} onClick={handleClick}>{title}</button>
   )
 }
