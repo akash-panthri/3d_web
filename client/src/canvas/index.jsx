582af96ebaf1f393fe071c95f427834946941c5a
akash-panthri <akashpanthri2000@gmail.com>
2024-08-29 19:20:15 +0530

    Added Shirt.jsx

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,9 +1,13 @@
 import {Canvas} from '@react-three/fiber'
 import {Environment , Center} from '@react-three/drei'
 
+import Shirt from './Shirt'
+import CameraRig from './CameraRig'
+import Backdrop from './Backdrop'
+
 const CanvasModel = () => {
   return (
-    <div>CanvasModel</div>
+    <Canvas>CanvasModel</Canvas>
   )
 }
 
