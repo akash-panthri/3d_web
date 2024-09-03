5ef821bcb3186385701278320f98f27a5f16fbec
akash-panthri <akashpanthri2000@gmail.com>
2024-09-03 07:45:57 +0530

    Added code for outline type in CustomButton.jsx

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -9,6 +9,13 @@ export default function CustomButton({type, title, customStyles,handleClick}) {
     if(type === 'filled'){
       return {backgroundColor: snap.color, color: 'white'}
     }
+    else if(type === "outline") {
+      return {
+        borderWidth: '1px',
+        borderColor: snap.color,
+        color: snap.color
+      }
+    }
   }
   return (
     <button style={generateStyle(type)} className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`} onClick={handleClick}>{title}</button>
