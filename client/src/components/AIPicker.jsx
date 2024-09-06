a040dae75f5286209d273f0ca2d6b8d508784a0c
akash-panthri <akashpanthri2000@gmail.com>
2024-09-06 07:20:49 +0530

    Added textarea to AIPicker.jsx

--- client/src/components/AIPicker.jsx
+++ client/src/components/AIPicker.jsx
@@ -1,7 +1,20 @@
+import CustomButton from "./CustomButton";
 
-
-export default function AIPicker() {
+export default function AIPicker({
+  prompt,
+  setPrompt,
+  generatingImg,
+  handleSubmit,
+}) {
   return (
-    <div>AIPicker</div>
-  )
+    <div className="aipicker-container">
+      <textarea
+        placeholder="Ask AI..."
+        rows={5}
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+        className="aipicker-textarea"
+      />
+    </div>
+  );
 }
